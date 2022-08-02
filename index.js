eb2d65b3ca31721d6fe19e07213848325ce83505
williamjordan <will.jordan0788@gmail.com>
2022-08-02 14:07:45 -0700

    updated the click function

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
+let newQuote = document.querySelector('#new-quote-btn')
+function fetchQuotes() { 
 fetch("https://api.quotable.io/random")
   .then((res) => res.json())
   .then((quoteData) => {
     renderQuote(quoteData);
   });
+}
 
 function renderQuote(quoteData) {
-  const p = document.querySelector("blockquote p");
-  p.textContent = `"${quoteData.content}"`;
-  const author = document.querySelector("#author");
-  author.textContent = `━ ${quoteData.author}`;
-}
+    
+        const p = document.querySelector("blockquote p");
+        p.textContent = `"${quoteData.content}"`;
+        const author = document.querySelector("#author");
+        author.textContent = `━ ${quoteData.author}`;
+    }
+
+newQuote.addEventListener('click', () => fetchQuotes())
+
+fetchQuotes()
