b9c64ddeef0728840613d0ce08f12ee46ec2227a
Kristina <kristina.hodges7@gmail.com>
2022-08-03 13:13:47 -0400

    fetched search results by author

--- index.js
+++ index.js
@@ -19,15 +19,60 @@ function renderQuote(quoteData) {
   });
 }
 
-fetch("https://api.quotable.io/tags")
-  .then((res) => res.json())
-  .then((list) => renderTags(list));
+let form = document.querySelector("#search-form");
+form.addEventListener("input", (e) => {
+  e.preventDefault();
+  let searchResult = document.querySelector("#search-bar").value.toLowerCase();
+  let searchContainer = document.querySelector("#searchResultContainer");
+  searchContainer.textContent = "";
+  if (searchResult === "") {
+    console.log("undefined");
+  } else {
+    fetch(
+      `http://api.quotable.io/search/quotes?query=${searchResult}&fields=author&limit=3`
+    )
+      .then((res) => res.json())
+      .then((searchData) => {
+        searchData.results.forEach((quote) => {
+          let quotesUL = document.createElement("ul");
+          let quotesLI = document.createElement("li");
+          quotesLI.textContent = `${quote.content} by ${quote.author}`;
+          quotesUL.append(quotesLI);
+          searchContainer.append(quotesUL, quotesLI);
+        });
+      });
+  }
+});
 
-function renderTags(list) {
-  list.forEach((tag) => {
-    const ul = document.querySelector("div ul");
-    const li = document.createElement("li");
-    li.textContent = tag.name;
-    ul.append(li);
-  });
-}
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let searchResult = document.querySelector("#search-bar").value.toLowerCase();
+  let searchContainer = document.querySelector("#searchResultContainer");
+  searchContainer.textContent = "";
+  fetch(
+    `http://api.quotable.io/search/quotes?query=${searchResult}&fields=author`
+  )
+    .then((res) => res.json())
+    .then((searchData) => {
+      searchData.results.forEach((quote) => {
+        let quotesUL = document.createElement("ul");
+        let quotesLI = document.createElement("li");
+        quotesLI.textContent = `${quote.content} by ${quote.author}`;
+        quotesUL.append(quotesLI);
+        searchContainer.append(quotesUL, quotesLI);
+      });
+    });
+});
+
+// fetch("https://api.quotable.io/tags")
+//   .then((res) => res.json())
+//   .then((list) => renderTags(list));
+
+// function renderTags(list) {
+//   list.forEach((tag) => {
+//     const ul = document.querySelector("div ul");
+//     const li = document.createElement("li");
+//     li.textContent = tag.name;
+//     ul.append(li);
+//   });
+// }
